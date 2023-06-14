fb2a7f2f7c19cf46e3f057d87ee917a024b90410
Miguel <miguelpechero80@gmail.com>
2023-06-14 19:55:08 +0200

    added login view and button password

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,26 +1,80 @@
 <template>
     <main>
-        <h1>Hola Login</h1>
-        <form>
-            <label for="">Usuario</label>
-            <input type="text">
-            <label for="">Contraseña</label>
-            <input type="text">
-            <button type="submit">Login</button>
-        </form>
+      <h1>Hola Login</h1>
+      <form @submit.prevent="login">
+        <label for="username">Usuario</label>
+        <input type="text" id="username" v-model="username">
+        <label for="password">Contraseña</label>
+        <div class="password-container">
+          <input
+            :type="showPassword ? 'text' : 'password'"
+            id="password"
+            v-model="password"
+          >
+          <button @click="togglePasswordVisibility" class="toggle-password-button">
+            {{ showPassword ? 'Ocultar' : 'Mostrar' }} contraseña
+          </button>
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </main>
-</template>
-
-<style>
-form {
+  </template>
+  
+  <style>
+  main {
     display: flex;
     flex-direction: column;
     align-items: center;
-}
-form input, form button {
+    padding: 5rem;
+  }
+  
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  form input,
+  form button {
     margin: 0.2rem;
-}
-form label {
+  }
+  
+  form label {
     align-self: flex-start;
-}
-</style>
+  }
+  
+  .password-container {
+    position: relative;
+  }
+  
+  .toggle-password-button {
+    color: #ffff;
+    border: 2px solid #ff556e;
+    font-size: 14px;
+    font-weight: 500;
+    border-radius: 28px;
+    background-color: #ff556e;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  </style>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+        showPassword: false
+      };
+    },
+    methods: {
+      login() {
+      },
+      togglePasswordVisibility() {
+        this.showPassword = !this.showPassword;
+      }
+    }
+  };
+  </script>
